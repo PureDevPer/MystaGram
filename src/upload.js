09e227dae00c925a96f637109095d98dce35b95c
PureDevPer <wooseok.kim1207@gmail.com>
2019-08-30 18:06:10 -0400

    Connected to AWS S3

--- src/upload.js
+++ src/upload.js
@@ -1,11 +1,30 @@
 import multer from 'multer';
+import multerS3 from 'multer-s3';
+import aws from 'aws-sdk';
 
-const upload = multer({ dest: 'uploads/' });
+const s3 = new aws.S3({
+	accessKeyId: process.env.AWS_KEY,
+	secretAccessKey: process.env.AWS_SECRET
+});
+
+const upload = multer({
+	storage: multerS3({
+		s3,
+		bucket: 'mystagram-wooseok',
+		metadata: function(req, file, cb) {
+			cb(null, { fieldName: file.fieldname });
+		},
+		key: function(req, file, cb) {
+			cb(null, Date.now().toString());
+		}
+	})
+});
 export const uploadMiddleware = upload.single('file');
 
 export const uploadController = (req, res) => {
 	const {
-		file: { path }
+		file: { location }
 	} = req;
-	res.json({ path });
+
+	res.json({ location });
 };
